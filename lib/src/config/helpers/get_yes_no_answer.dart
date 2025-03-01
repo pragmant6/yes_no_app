35e536de382e40db1a2962bd6f59018500c7eea1
Miguel Angel Prado Garcia <mprado@mosbit.com>
2025-03-01 16:46:56 -0600

    se agregaron paquetes

---
+++ lib/src/config/helpers/get_yes_no_answer.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+
+import 'package:yes_no_app/src/domain/entities/message.dart';
+
+class GetYesNoAnswer {
+  final _dio = Dio();
+  Future<Message> getAnswer() async {
+    final response = await _dio.get('https://yesno.wtf/api');
+
+    return Message(
+      text: response.data['answer'],
+      fromWho: FromWho.hers,
+      imageUrl: response.data['image'],
+    );
+  }
+}
--- lib/src/presentation/providers/chat_provider.dart
+++ lib/src/presentation/providers/chat_provider.dart
@@ -1,15 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:yes_no_app/src/config/helpers/get_yes_no_answer.dart';
+
 import 'package:yes_no_app/src/domain/entities/message.dart';
 
 class ChatProvider extends ChangeNotifier {
   final ScrollController chatScrollController = ScrollController();
+  final getYesNoAnswer = GetYesNoAnswer();
   List<Message> messagesList = [];
 
   void sendMessage(String message) async {
     if (message.isEmpty) return;
-    final newMessage = Message(text: message, fromWho: FromWho.my);
-    messagesList.add(newMessage);
+    messagesList.add(Message(text: message, fromWho: FromWho.my));
+    if (message.endsWith('?')) {
+      await herReply();
+    }
+
     notifyListeners();
+    moveStrollToBottom();
+  }
+
+  Future<void> herReply() async {
+    final herMessage = await GetYesNoAnswer.getAnswer();
+    messagesList.add(herMessage);
+    notifyListeners();
+    moveStrollToBottom();
   }
 
   void moveStrollToBottom() async {
